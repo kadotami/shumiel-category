11fef012b5b3fed291244e8ea771db753824d8bc
kadotami <mondamin.kqfb@gmail.com>
2017-07-16 15:19:37 +0900

    本番適応変更

--- word2vec-api.py
+++ word2vec-api.py
@@ -3,7 +3,8 @@
 from flask import Flask, request, jsonify
 from flask.ext.restful import Resource, Api, reqparse
 from flask_restful.utils import cors
-from gensim.models.word2vec import Word2Vec as w
+# from gensim.models.word2vec import Word2Vec as w
+from gensim.models.keyedvectors import KeyedVectors as w
 import argparse
 import base64
 import sys
@@ -30,7 +31,7 @@ class Category(Resource):
             parser = reqparse.RequestParser()
             parser.add_argument('query', required=True, help="query cannot be blank!")
             args = parser.parse_args()
-            mecab = MeCab.Tagger('-Owakati -d /opt/local/lib/mecab/dic/mecab-ipadic-neologd')
+            mecab = MeCab.Tagger('-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
             query = args['query']
             mecab.parse('')
             node = mecab.parseToNode(query)
@@ -53,36 +54,45 @@ class Category(Resource):
             return e
 
     def get(self):
-        categories = [
-            u"読書",
-            u"スポーツ",
-            u"ゲーム",
-            u"音楽",
-            u"グルメ",
-            u"旅行",
-            u"アニメ",
-            u"アウトドア",
-            u"健康",
-        ]
-        cat_val = defaultdict(lambda: 0)
-        parser = reqparse.RequestParser()
-        parser.add_argument('query', type=unicode, required=True, help="query cannot be blank!")
-        args = parser.parse_args()
-        words = []
-        mecab = MeCab.Tagger('-Owakati -d /opt/local/lib/mecab/dic/mecab-ipadic-neologd')
-        query = args['query']
-        return query
-        
-        node = mecab.parseToNode(query)
-        while node:
-            if node.feature.split(",")[0] == u'名詞':
-                words.append(node.surface)
-            node = node.next
-        for cat in categories:
-            for word in words:
-                cat_val[cat] += model.similarity(word, cat)
-            cat_val[cat] = cat_val[cat]/len(words)
-        return cat_val
+        try:
+            categories = [
+                u"読書",
+                u"スポーツ",
+                u"ゲーム",
+                u"音楽",
+                u"グルメ",
+                u"旅行",
+                u"アニメ",
+                u"アウトドア",
+                u"健康",
+            ]
+            words = []
+            not_in_model = 0
+            cat_val = defaultdict(lambda: 0)
+            parser = reqparse.RequestParser()
+            parser.add_argument('query', required=True, help="query cannot be blank!")
+            args = parser.parse_args()
+            mecab = MeCab.Tagger('-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+            query = args['query']
+            mecab.parse('')
+            node = mecab.parseToNode(query)
+            while node:
+                if node.feature.split(",")[0] == u'名詞':
+                    words.append(node.surface)
+                node = node.next
+            for cat in categories:
+                for word in words:
+                    try:
+                        cat_val[cat] += model.similarity(word, cat)
+                    except:
+                        not_in_model += 1
+                if ((len(words) - not_in_model) != 0):
+                    cat_val[cat] = cat_val[cat]/(len(words) - not_in_model)
+                else:
+                    cat_val[cat] = 0
+            return cat_val
+        except(Exception, e):
+            return e
 
 app = Flask(__name__)
 api = Api(app)
